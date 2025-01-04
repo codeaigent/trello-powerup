e8129e8b2348c2bb3f0d878a682db7fa19dff520
BaradwajVaradharajan-TomTom <baradwaj.varadharajan@tomtom.com>
2025-01-04 22:42:53 +0100

    simple card-buttons capability

--- client.js
+++ client.js
@@ -1,73 +1,35 @@
 /* global TrelloPowerUp */
 
-// we can access Trello's deferred object through `TrelloPowerUp.Promise`
-const t = TrelloPowerUp.iframe();
+console.log('Power-Up initializing...'); // Debug log
 
 // This is the main initialization function that Trello will call
 window.TrelloPowerUp.initialize({
-  // Start adding your Power-Up functionality here
-  'board-buttons': function(t, options) {
+  'card-buttons': function(t, options) {
+    console.log('card-buttons capability called', options); // Debug log
     return [{
       icon: {
         dark: './icon-dark.png',
         light: './icon-light.png'
       },
-      text: 'Sample Button',
+      text: 'Card Action',
       callback: function(t) {
-        return t.modal({
-          title: 'Sample Modal',
-          url: './index.html',
-          height: 500
-        });
-      }
-    }];
-  },
-  
-  'card-badges': function(t, options) {
-    return t.card('all')
-    .then(function(card) {
-      return [{
-        text: '👋',
-        color: 'blue',
-      }];
-    });
-  },
-
-  'card-buttons': function(t, options) {
-    return [{
-      icon: './icon.png',
-      text: 'Open Card',
-      callback: function(t) {
+        console.log('card button clicked'); // Debug log
         return t.popup({
           title: 'Card Actions',
           items: [
             {
               text: 'Action 1',
               callback: function(t) {
-                // Add your action here
+                console.log('Action 1 clicked'); // Debug log
+                return t.alert({
+                  message: 'Action clicked!',
+                  duration: 3
+                });
               }
             }
           ]
         });
       }
     }];
-  },
-
-  'show-settings': function(t, options) {
-    return t.popup({
-      title: 'Settings',
-      items: [
-        {
-          text: 'Configure Power-Up',
-          callback: function(t) {
-            return t.modal({
-              title: 'Power-Up Settings',
-              url: './settings.html',
-              height: 300
-            });
-          }
-        }
-      ]
-    });
   }
 }); 
