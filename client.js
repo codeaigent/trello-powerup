21d81a7ebe3847765a3a1a7b177e74a6b2a87fc2
barath-uni <diputs.creators@gmail.com>
2025-01-05 17:09:26 +0100

    changes to get the authorization to work

--- client.js
+++ client.js
@@ -1,12 +1,10 @@
 /* global TrelloPowerUp */
 
 console.log('CodeAIgent Power-Up initializing...'); // Debug log
-
 // API key is defined in config.js which is generated during deployment
 if (!POWERUP_API_KEY) {
-  console.error('API key not found. Make sure config.js is properly generated.');
+    console.error('API key not found. Make sure config.js is properly generated.');
 }
-
 // Function to add bot to board
 function addBotToBoard(t, token) {
   return t.board('id')
@@ -41,24 +39,24 @@ function addBotToBoard(t, token) {
 // This is the main initialization function that Trello will call
 window.TrelloPowerUp.initialize({
   'authorization-status': function(t, options) {
-    return t.get('member', 'private', 'token')
-      .then(function(token) {
-        return { authorized: token != null };
-      });
+    return t.get('member', 'private', 'authToken')
+    .then(function(authToken) {
+      return { authorized: authToken != null }
+    });
   },
 
   'show-authorization': function(t, options) {
     return t.popup({
       title: 'CodeAIgent Authorization',
       url: './auth.html',
-      height: 180
+      height: 140,
     });
   },
 
   'card-buttons': function(t, options) {
-    return t.get('member', 'private', 'token')
-      .then(function(token) {
-        if (!token) {
+    return t.get('member', 'private', 'authToken')
+      .then(function(authToken) {
+        if (!authToken) {
           return [{
             icon: {
               dark: './icon-dark.png',
@@ -69,7 +67,7 @@ window.TrelloPowerUp.initialize({
               return t.popup({
                 title: 'CodeAIgent Authorization',
                 url: './auth.html',
-                height: 180
+                height: 140
               });
             }
           }];
@@ -93,7 +91,19 @@ window.TrelloPowerUp.initialize({
                       .then(function(card) {
                         console.log('Adding comment to card:', card.id);
                         // First ensure bot is added to board
-                        return addBotToBoard(t, token)
+                        return addBotToBoard(t, authToken)
+                          .then(function() {
+                            // Add comment using Trello API
+                            return fetch(`https://api.trello.com/1/cards/${card.id}/actions/comments?key=${POWERUP_API_KEY}&token=${authToken}`, {
+                              method: 'POST',
+                              headers: {
+                                'Content-Type': 'application/json',
+                              },
+                              body: JSON.stringify({
+                                text: '@codeaigent Please help with this task'
+                              })
+                            });
+                          })
                           .then(function() {
                             return t.set('card', 'shared', 'lastComment', {
                               text: '@codeaigent Please help with this task',
