1c8f5a6dd8399a1dcc1f318b7c1e48ab87068851
barath-uni <barath-uni@users.noreply.github.com>
2025-01-06 12:26:53 +0000

    deploy: 2e0ead822a6aa1eb7194ef929e0de000784efbc6

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
-const POWERUP_API_KEY = '406a8bcb2d5033945fbc4d69dc0f5af8'
-const BOT_EMAIL = 'bot@codeaigent.com'
+// Auto-generated config file - DO NOT EDIT
+const POWERUP_API_KEY = '406a8bcb2d5033945fbc4d69dc0f5af8';
+const BOT_EMAIL = 'bot@codeaigent.com';
+
+// Validate required configuration
+if (!POWERUP_API_KEY) throw new Error('TRELLO_API_KEY is required');
+if (!BOT_EMAIL) throw new Error('BOT_EMAIL is required');
+if (!BACKEND_URL) throw new Error('BACKEND_URL is required');
