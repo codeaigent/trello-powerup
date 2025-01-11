22da67f8583d3c49012ed52cd5189ad5a565fe30
barath-uni <barath-uni@users.noreply.github.com>
2025-01-11 20:12:23 +0000

    deploy: 87d0f8809e5d403ef2a53ff669ab005dd3be5fbd

--- js/authorize-github.js
+++ js/authorize-github.js
@@ -7,7 +7,7 @@ authBtn.addEventListener('click', function() {
   const githubAuthUrl = 'https://github.com/login/oauth/authorize';
   const clientId = `${CLIENT_ID}`;
   const scope = 'repo read:user';
-  const redirectUri = `${window.location.origin}/auth-success.html`;
+  const redirectUri = `${window.location.origin}/github-auth-success.html`;
   const state = Math.random().toString(36).substring(7); // Generate random state
 
   // Store state for validation
